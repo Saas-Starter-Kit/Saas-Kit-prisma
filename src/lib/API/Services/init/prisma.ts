585b0b21c9ba071512b1125c275d1d0a143485d1
iqbal125 <iqbal125sf@gmail.com>
2023-11-02 13:09:49 -0700

    fix prisma url

--- src/lib/API/Services/init/prisma.ts
+++ src/lib/API/Services/init/prisma.ts
@@ -1,7 +1,9 @@
 import 'server-only';
 import { Prisma } from '@prisma/client';
+//import { PrismaClient } from '@prisma/client/edge';
+//import { withAccelerate } from '@prisma/extension-accelerate';
+
 import { PrismaClient } from '@prisma/client/edge';
-import { withAccelerate } from '@prisma/extension-accelerate';
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -12,10 +14,12 @@ import { withAccelerate } from '@prisma/extension-accelerate';
 let prisma;
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient().$extends(withAccelerate());
+  //prisma = new PrismaClient().$extends(withAccelerate());
+  prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient().$extends(withAccelerate());
+    //global.prisma = new PrismaClient().$extends(withAccelerate());
+    global.prisma = new PrismaClient();
   }
   prisma = global.prisma;
 }
