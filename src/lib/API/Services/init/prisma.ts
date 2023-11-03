6595dca691c95ad3c31bfa490ac9cb54ad44108b
iqbal125 <iqbal125sf@gmail.com>
2023-11-02 19:23:27 -0700

    revert edge

--- src/lib/API/Services/init/prisma.ts
+++ src/lib/API/Services/init/prisma.ts
@@ -1,9 +1,9 @@
 import 'server-only';
 import { Prisma } from '@prisma/client';
-import { PrismaClient } from '@prisma/client/edge';
-import { withAccelerate } from '@prisma/extension-accelerate';
+//import { PrismaClient } from '@prisma/client/edge';
+//import { withAccelerate } from '@prisma/extension-accelerate';
 
-//import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -14,12 +14,12 @@ import { withAccelerate } from '@prisma/extension-accelerate';
 let prisma;
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient().$extends(withAccelerate());
-  //prisma = new PrismaClient();
+  //prisma = new PrismaClient().$extends(withAccelerate());
+  prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient().$extends(withAccelerate());
-    //global.prisma = new PrismaClient();
+    //global.prisma = new PrismaClient().$extends(withAccelerate());
+    global.prisma = new PrismaClient();
   }
   prisma = global.prisma;
 }
