d2f1a5ebeb28785e02c66346c799fd1d3ff9954c
iqbal125 <iqbal125sf@gmail.com>
2023-11-02 18:56:57 -0700

    remove nodemailer

--- src/lib/API/Services/auth/auth.config.ts
+++ src/lib/API/Services/auth/auth.config.ts
@@ -8,17 +8,17 @@ export default {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    }),
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD
-        }
-      },
-      from: process.env.EMAIL_FROM
     })
+    //EmailProvider({
+    //  server: {
+    //    host: process.env.EMAIL_SERVER_HOST,
+    //    port: process.env.EMAIL_SERVER_PORT,
+    //    auth: {
+    //      user: process.env.EMAIL_SERVER_USER,
+    //      pass: process.env.EMAIL_SERVER_PASSWORD
+    //    }
+    //  },
+    //  from: process.env.EMAIL_FROM
+    //})
   ]
 } satisfies NextAuthConfig;
